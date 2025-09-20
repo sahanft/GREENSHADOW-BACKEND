2852b69cdc8f029495e492b7bb4b9c4158ec6634
sahanft <udakarafernando2001@gmail.com>
2025-09-20 21:24:37 +0530

    modify the healthchecker controller

--- src/main/java/lk/ijse/greenshadow/controller/HealthCheckController.java
+++ src/main/java/lk/ijse/greenshadow/controller/HealthCheckController.java
@@ -1,9 +1,9 @@
 package lk.ijse.greenshadow.controller;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import lk.ijse.greenshadow.util.AppUtil;
+import lk.ijse.greenshadow.util.ResponseUtil;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
 
 @CrossOrigin
 @RestController
@@ -16,3 +16,21 @@ public class HealthCheckController {
 }
 
 
+
+    @PreAuthorize("hasRole('MANAGER') or hasRole('ADMINISTRATIVE') or hasRole('SCIENTIST')")
+    @GetMapping
+    public ResponseUtil getAllEquipments() {
+        return new ResponseUtil("Success", "Retrieved All Equipments", equipmentService.getAllEquipments());
+    }
+
+    @PreAuthorize("hasRole('MANAGER') or hasRole('ADMINISTRATIVE')")
+    @GetMapping("/newid")
+    public ResponseUtil getNewEquipmentId() {
+        return new ResponseUtil("Success", "Retrieved New Equipment Id", AppUtil.generateEquipmentId(equipmentService.findLastEquipmentId()));
+    }
+
+    @PreAuthorize("hasRole('MANAGER') or hasRole('ADMINISTRATIVE') or hasRole('SCIENTIST')")
+    @GetMapping("/{equipmentId}")
+    public ResponseUtil getEquipmentById(@PathVariable("equipmentId") String equipmentId) {
+        return new ResponseUtil("Success", "Retrieved Equipment", equipmentService.getEquipmentById(equipmentId));
+    }
