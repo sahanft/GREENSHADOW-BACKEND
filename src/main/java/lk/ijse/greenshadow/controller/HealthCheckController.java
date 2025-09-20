80dbbd82b56ed0a9e35bf364bf4f5f8b8ddea083
sahanft <udakarafernando2001@gmail.com>
2025-09-20 21:29:14 +0530

    modify the healthchecker controller

--- src/main/java/lk/ijse/greenshadow/controller/HealthCheckController.java
+++ src/main/java/lk/ijse/greenshadow/controller/HealthCheckController.java
@@ -1,7 +1,13 @@
 package lk.ijse.greenshadow.controller;
 
+import lk.ijse.greenshadow.dto.FieldStaffDTO;
+import lk.ijse.greenshadow.dto.StaffDTO;
+import lk.ijse.greenshadow.service.StaffService;
 import lk.ijse.greenshadow.util.AppUtil;
 import lk.ijse.greenshadow.util.ResponseUtil;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
@@ -14,8 +20,67 @@ public class HealthCheckController {
         return "Healthy";
     }
 }
+//---------------------------------------------------------------------
 
+    private StaffService staffService;
+    @PreAuthorize("hasRole('MANAGER') or hasRole('ADMINISTRATIVE')")
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Void> saveStaff(@RequestBody StaffDTO staffDTO) {
+        staffService.saveStaff(staffDTO);
+        return new ResponseEntity<>(HttpStatus.CREATED);
+    }
 
+    @PreAuthorize("hasRole('MANAGER') or hasRole('ADMINISTRATIVE')")
+    @PutMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Void> updateStaff(@RequestBody StaffDTO staffDTO) {
+        staffService.updateStaff(staffDTO);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+
+    @PreAuthorize("hasRole('MANAGER') or hasRole('ADMINISTRATIVE')")
+    @DeleteMapping(path = "/{staffId}")
+    public ResponseEntity<Void> deleteStaff(@PathVariable String staffId) {
+        staffService.deleteStaff(staffId);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+
+    @PreAuthorize("hasRole('MANAGER') or hasRole('ADMINISTRATIVE') or hasRole('SCIENTIST')")
+    @GetMapping
+    public ResponseUtil getAllStaffs() {
+        return new ResponseUtil("Success", "Retrieved All Staffs", staffService.getAllStaffs());
+    }
+
+    @PreAuthorize("hasRole('MANAGER') or hasRole('ADMINISTRATIVE')")
+    @PostMapping(value = "/fieldstaff", consumes = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Void> saveFieldStaff(@RequestBody FieldStaffDTO fieldStaffDTO) {
+        staffService.saveFieldStaff(fieldStaffDTO);
+        return new ResponseEntity<>(HttpStatus.CREATED);
+    }
+
+    @PreAuthorize("hasRole('MANAGER') or hasRole('ADMINISTRATIVE')")
+    @DeleteMapping("/fieldstaff")
+    public ResponseEntity<Void> deleteFieldStaff(@RequestParam("fieldCode") String fieldCode, @RequestParam("staffId") String staffId) {
+        staffService.deleteFieldStaff(fieldCode, staffId);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+
+    @PreAuthorize("hasRole('MANAGER') or hasRole('ADMINISTRATIVE')")
+    @GetMapping("/newid")
+    public ResponseUtil getNewStaffId() {
+        return new ResponseUtil("Success", "Retrieved New Staff Id", AppUtil.generateStaffId(staffService.findLastStaffId()));
+    }
+
+    @PreAuthorize("hasRole('MANAGER') or hasRole('ADMINISTRATIVE') or hasRole('SCIENTIST')")
+    @GetMapping("/{staffId}")
+    public ResponseUtil getStaffById(@PathVariable("staffId") String staffId) {
+        return new ResponseUtil("Success", "Retrieved Staff", staffService.getStaffById(staffId));
+    }
+
+    @PreAuthorize("hasRole('MANAGER') or hasRole('ADMINISTRATIVE') or hasRole('SCIENTIST')")
+    @GetMapping("/fieldstaff/{fieldCode}")
+    public ResponseUtil getFieldStaffByFieldCode(@PathVariable("fieldCode") String fieldCode) {
+        return new ResponseUtil("Success", "Retrieved Field Staff", staffService.getFieldStaffByFieldCode(fieldCode));
+    }
 
 
 
