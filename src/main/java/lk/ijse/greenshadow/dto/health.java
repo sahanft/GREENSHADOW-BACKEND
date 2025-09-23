c332d8d3f2810161bd524eff91997658538095ae
sahanft <udakarafernando2001@gmail.com>
2025-09-23 21:10:38 +0530

    create health entity

--- src/main/java/lk/ijse/greenshadow/dto/health.java
+++ src/main/java/lk/ijse/greenshadow/dto/health.java
@@ -5,6 +5,14 @@ import java.time.LocalDateTime;
 public class HealthRecordDTO {
 
 
+    private Long id;
+
+
+    @NotBlank(message = "serviceName is required")
+    @Size(max = 100)
+    private String serviceName;
+
+
 
 }
 
