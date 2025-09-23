d3e59e5980d02b9f252e6610e2b73194167da644
sahanft <udakarafernando2001@gmail.com>
2025-09-23 20:47:17 +0530

    create health entity

--- src/main/java/lk/ijse/greenshadow/entity/healthEntity.java
+++ src/main/java/lk/ijse/greenshadow/entity/healthEntity.java
@@ -1,4 +1,19 @@
 package lk.ijse.greenshadow.entity;
 
-public class healthEntity {
+
+        import org.hibernate.annotations.CreationTimestamp;
+        import org.hibernate.annotations.UpdateTimestamp;
+
+
+        import javax.persistence.*;
+        import java.time.LocalDateTime;
+
+
+@Entity
+@Table(name = "health_record")
+public class healthEntity  {
+
+
+
+
 }
